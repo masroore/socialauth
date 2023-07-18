41a4b28c461c89c7294504b466a900e291d9c13e
Masroor Ehsan <masroore@gmail.com>
2023-07-18 09:44:14 +0600

    refactorings

--- src/Support/Routes.php
+++ src/Support/Routes.php
@@ -6,13 +6,21 @@ use Masroore\SocialAuth\SocialAuth;
 
 final class Routes
 {
-    public static function redirect(string $routeName, string $default): string
+    public static function redirect(string $routeName, ?string $default = null): string
     {
-        return config(SocialAuth::PACKAGE_NAME . '.redirects.' . $routeName) ?? $default;
+        return self::config('redirects', $routeName, $default);
     }
 
-    public static function for(string $routeName, string $default): string
+    private static function config(string $section, string $routeName, ?string $default = null): string
     {
-        return config(SocialAuth::PACKAGE_NAME . '.routes.' . $routeName) ?? $default;
+        $default ??= $routeName;
+        $key = implode('.', [SocialAuth::PACKAGE_NAME, $section, $routeName]);
+
+        return (string) (config($key) ?? $default);
+    }
+
+    public static function for(string $routeName, ?string $default = null): string
+    {
+        return self::config('routes', $routeName, $default);
     }
 }
