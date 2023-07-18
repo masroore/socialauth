ff0490b4f262a54bfce7c5ee27ad48af4be07670
Masroor Ehsan <masroore@gmail.com>
2023-07-18 11:55:27 +0600

    removed domain from route

--- routes/socialauth.php
+++ routes/socialauth.php
@@ -3,8 +3,8 @@
 use Masroore\SocialAuth\Http\Controllers\SocialAuthController;
 use Masroore\SocialAuth\SocialAuth;
 
-Route::domain(get_config('domain', 'localhost'))
-    ->middleware(get_config('middleware', ['web']))
+Route::middleware(get_config('middleware', ['web']))
+    // ->domain(get_config('domain', 'localhost'))
     ->name(SocialAuth::PACKAGE_NAME . '.')
     ->group(static function (): void {
         Route::get('/oauth/{provider}', [SocialAuthController::class, 'redirectToProvider'])
