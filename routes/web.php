fad176272d44410c2041388e7bd8ab29a76f2ff7
Masroor Ehsan <masroore@gmail.com>
2023-07-18 09:57:15 +0600

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use Masroore\SocialAuth\Http\Controllers\SocialAuthController;
+use Masroore\SocialAuth\SocialAuth;
 
-Route::domain(config('socialauth.domain'))
-    ->middleware(config('socialauth.middleware', ['web']))
-    ->name('socialauth.')
+Route::domain(get_config('domain', 'localhost'))
+    ->middleware(get_config('middleware', ['web']))
+    ->name(SocialAuth::PACKAGE_NAME . '.')
     ->group(static function (): void {
         Route::get('/oauth/{provider}', [SocialAuthController::class, 'redirectToProvider'])
             ->name('redirect');
--- src/helpers.php
+++ src/helpers.php
@@ -7,7 +7,7 @@ function config_key(?string $name = null): string
     return blank($name) ? SocialAuth::PACKAGE_NAME : SocialAuth::PACKAGE_NAME . '.' . $name;
 }
 
-function get_config(string $key, mixed $default): mixed
+function get_config(string $key, mixed $default = null): mixed
 {
     return config(config_key($key), $default);
 }
